b2713e9495c0a567bdac09f231b7ef20e6fdc2df
Bruno Vicente <brunnovicente@gmail.com>
2025-07-09 17:10:20 -0300

    Reabertura de ingressos

--- routes/aluno.js
+++ routes/aluno.js
@@ -2,8 +2,8 @@ import express from 'express';
 const router = express.Router();
 import AlunoController from '../controllers/AlunoController.js';
 
-//router.get('/confirmar', AlunoController.confirmar)
-//router.post('/presenca', AlunoController.presenca)
+router.get('/confirmar', AlunoController.confirmar)
+router.post('/presenca', AlunoController.presenca)
 router.get('/buscar/:matricula', AlunoController.buscar)
 router.get('/ingresso/:aluno', AlunoController.ingresso)
 router.get('/curso', AlunoController.curso)
