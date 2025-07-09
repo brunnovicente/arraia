5c34b901910b09cdf9dda0209abb98d69abb4acc
Bruno Vicente <brunnovicente@gmail.com>
2025-07-09 14:21:57 -0300

    Inscrições encerradas

--- routes/aluno.js
+++ routes/aluno.js
@@ -2,13 +2,13 @@ import express from 'express';
 const router = express.Router();
 import AlunoController from '../controllers/AlunoController.js';
 
-router.get('/confirmar', AlunoController.confirmar)
-router.post('/presenca', AlunoController.presenca)
+//router.get('/confirmar', AlunoController.confirmar)
+//router.post('/presenca', AlunoController.presenca)
 router.get('/buscar/:matricula', AlunoController.buscar)
 router.get('/ingresso/:aluno', AlunoController.ingresso)
 router.get('/curso', AlunoController.curso)
 router.get('/lista/:curso', AlunoController.lista)
-router.get('/egresso', (req, res)=> res.render('aluno/egresso'))
-router.post('/egresso', AlunoController.egresso)
+//router.get('/egresso', (req, res)=> res.render('aluno/egresso'))
+//router.post('/egresso', AlunoController.egresso)
 
 export default router;
